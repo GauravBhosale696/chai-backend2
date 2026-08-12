336a58c949d3c86f82d6e439ca0f95c61b67aed9
GauravBhosale696 <bhosalegaurav39@gmail.com>
2026-08-12 11:19:52 +0530

    connect to mongoatlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv"
+
 import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
-import connectDB from "./db/index.db";
+import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.db.js";
+
+dotenv.config({
+    path: './.env'
+})
+
 
 connectDB();
 
