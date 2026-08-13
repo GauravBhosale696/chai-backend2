a35b0907ed4cd440d4b0ff097d516d3c9e97ff92
GauravBhosale696 <bhosalegaurav39@gmail.com>
2026-08-13 21:59:21 +0530

    add register controller and bug fixes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-const upload = multer({ dest: 'uploads/' })
+// const upload = multer({ dest: 'uploads/' })
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
