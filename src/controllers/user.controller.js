24a8bf7df5619c57327bc6f83a878e85622026cd
GauravBhosale696 <bhosalegaurav39@gmail.com>
2026-08-14 10:35:26 +0530

    fixed cloudinary bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+
 import { ApiResponse } from "../utils/ApiResponse.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const registerUser = asyncHandler(async (req,res) => {
     // get user details from frontend 
     // validation - not empty
@@ -15,7 +16,7 @@ const registerUser = asyncHandler(async (req,res) => {
     // return res
 
     const {fullname, email, username, password} = req.body;
-    console.log("email", email);
+    // console.log("email", email);
 
     if(
         [fullname, email, username, password].some((field) => field?.trim() === "")
@@ -33,8 +34,14 @@ const registerUser = asyncHandler(async (req,res) => {
         throw new ApiError(409, "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar?.[0]?.path
-    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath= req.files.coverImage[0].path;
+    }
+
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,14 @@
+import dotenv from "dotenv"
+dotenv.config({
+    path: './.env'
+})
 import { v2 as cloudinary} from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY ? "FOUND" : "MISSING", 
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "FOUND" : "MISSING"
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
 }); 
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -15,12 +19,17 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         });
         //file has been uploaded successfully
-        console.log("File is uploaded on cloudinary", response.url );
+        // console.log("File is uploaded on cloudinary", response.url );
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch(error){
+    console.log("CLOUDINARY ERROR:", error);
+
+    if (fs.existsSync(localFilePath)) {
         fs.unlinkSync(localFilePath);
-        return null;
+    }
+    return null;
 };
     
 }
